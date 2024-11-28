b55a843057b2630100e62789c191fecc7eca5c91
kiranr7353 <62291859+kiranr7353@users.noreply.github.com>
2024-11-28 13:40:48 +0530

    latest changes

--- src/Envurl.js
+++ src/Envurl.js
@@ -1,54 +1,18 @@
 export const Envurl = () => {
     //identifying environments
     let domainUrl = window.location.hostname;
-    if (domainUrl.includes('uat')) {
+    if (domainUrl.includes('jsprepreact')) {
         return {
             url: domainUrl,
-            envName: 'eksuat',
-            apiURL: 'https://ehapp-uat.elevancehealth.com/smarthelp/smarthelpapiv2/',
+            envName: 'jsprepreact',
+            apiURL: 'https://jsprepbackend-production.up.railway.app/api/v1/',
             // oAuthRedirectURL: 'https://ehapp-uat.elevancehealth.com/app/appuidashboard/home',
             // contextPath: '/app/appuidashboard',
-            // siteUrl: 'https://ehapp-uat.elevancehealth.com/app/appuidashboard',
-            reactLogoutUrl: 'https://ehapp-uat.elevancehealth.com/app/appuidashboard/logout',
+            siteUrl: 'https://jsprepreact.onrender.com',
+            reactLogoutUrl: 'https://jsprepreact.onrender.com/logout',
             // reactUAMUrl: 'https://ehapp-uat.elevancehealth.com/app/appuiuamapp/access-management',
         };
     }
-    // else if (domainUrl.includes('sit')) {
-    //     return {
-    //         url: domainUrl,
-    //         envName: 'ekssit',
-    //         apiURL: 'https://ehapp-sit.elevancehealth.com/smarthelp/smarthelpapiv2/',
-    //         oAuthRedirectURL: 'https://ehapp-sit.elevancehealth.com/app/appuidashboard/home',
-    //         contextPath: '/app/appuidashboard',
-    //         siteUrl: 'https://ehapp-sit.elevancehealth.com/app/appuidashboard',
-    //         reactLogoutUrl: 'https://ehapp-sit.elevancehealth.com/app/appuidashboard/logout',
-    //         reactUAMUrl: 'https://ehapp-sit.elevancehealth.com/app/appuiuamapp/access-management',
-    //     };
-    // }
-    // else if (domainUrl.includes('perf')) {
-    //     return {
-    //         url: domainUrl,
-    //         envName: 'eksperf',
-    //         apiURL: 'https://ehapp-perf.elevancehealth.com/smarthelp/smarthelpapiv2/',
-    //         oAuthRedirectURL: 'https://ehapp-perf.elevancehealth.com/app/appuidashboard/home',
-    //         contextPath: '/app/appuidashboard',
-    //         siteUrl: 'https://ehapp-perf.elevancehealth.com/app/appuidashboard',
-    //         reactLogoutUrl: 'https://ehapp-perf.elevancehealth.com/app/appuidashboard/logout',
-    //         reactUAMUrl: 'https://ehapp-perf.elevancehealth.com/app/appuiuamapp/access-management'
-    //     };
-    // }
-    // else if (domainUrl.includes('ehapp.elevancehealth.com')) {
-    //     return {
-    //         url: domainUrl,
-    //         envName: 'production',
-    //         apiURL: 'https://ehapp.elevancehealth.com/smarthelp/smarthelpapiv2/',
-    //         oAuthRedirectURL: 'https://ehapp.elevancehealth.com/app/appuidashboard/home',
-    //         contextPath: '/app/appuidashboard',
-    //         siteUrl: 'https://ehapp.elevancehealth.com/app/appuidashboard',
-    //         reactLogoutUrl: 'https://ehapp.elevancehealth.com/app/appuidashboard/logout',
-    //         reactUAMUrl: 'https://ehapp.elevancehealth.com/app/appuiuamapp/access-management'
-    //     };
-    // }
     else if (domainUrl.includes('localhost')) {
         return {
             url: domainUrl,
