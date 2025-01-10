d1617063275520684c75de050defbf8a917d74a7
R <AH82741@ad.wellpoint.com>
2025-01-10 17:56:02 +0530

    latest changes

--- src/Components/App/AWS/AWSHome.js
+++ src/Components/App/AWS/AWSHome.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import IAM from './IAM';
 import AWSInterviewQA from './AWSInterviewQA';
+import AWSEC2InterviewQA from './AWSEC2InterviewQA';
+import AWSS3InterviewQA from './AWSS3InterviewQA';
+import AWSIAMInterviewQA from './AWSIAMInterviewQA';
 
 const AWSHome = (props) => {
 
@@ -12,6 +15,12 @@ const AWSHome = (props) => {
         return <IAM params={params} locationDetails={locationDetails} />
       case 'awsInterviewQA':
         return <AWSInterviewQA params={params} locationDetails={locationDetails} />
+      case 'awEC2InterviewQA':
+        return <AWSEC2InterviewQA params={params} locationDetails={locationDetails} />
+      case 'awS3InterviewQA':
+        return <AWSS3InterviewQA params={params} locationDetails={locationDetails} />
+      case 'awsIAMInterviewQA':
+        return <AWSIAMInterviewQA params={params} locationDetails={locationDetails} />
       default:
         return <IAM params={params} locationDetails={locationDetails} />
     }
